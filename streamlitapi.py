5dea7adce74ced6dddd2a4751cef85e65475d90d
Jucodez <102746691+Jucodez@users.noreply.github.com>
2024-09-19 15:39:52 +0100

    Update streamlitapi.py

--- streamlitapi.py
+++ streamlitapi.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 import os
 
-model_path = os.path.join(os.path.dirname(__file__), 'car_price_predictor.pkl.pkl')  # Change 'model.pkl' to your actual model filename
+model_path = os.path.join(os.path.dirname(__file__), 'car_price_predictor.pkl')  # Change 'model.pkl' to your actual model filename
 model = pickle.load(open(model_path, 'rb'))
 
 def main():
